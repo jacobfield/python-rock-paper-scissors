d20513214da3883605af2d90565f1ac239e54ce6
Jake Field <jacobprogrammes@gmail.com>
2024-06-10 15:27:25 +0100

    continued builsing

--- weather.py
+++ weather.py
@@ -1,3 +1,10 @@
 import requests
 
 api_key = "de813b00c655b51d656811e5394737d2"
+
+city = input("Where would you like to see the weather for?")
+
+url = (f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
+
+response = requests.get(url)
+if response.status.code == 200
