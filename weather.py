edfd7216a044c180f0471b2335f56cda456c8680
Jake Field <jacobprogrammes@gmail.com>
2024-06-10 15:56:01 +0100

    env vars now hidden

--- weather.py
+++ weather.py
@@ -1,17 +1,34 @@
 import requests
 import math
-api_key = "de813b00c655b51d656811e5394737d2"
+from configparser import ConfigParser
 
+# Read the API key from the config file
+config = ConfigParser()
+config.read('config.cfg')
+api_key = config.get('api_keys','api_key')
+
+# Prompt the user to enter the city for which they want to see the weather
 city = input("Where would you like to see the weather for?")
 
+# Create the URL for the weather API request
 url = (f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
 
+# Send the API request and get the response
 response = requests.get(url)
+
+# Check if the request was successful (status code 200)
 if response.status_code == 200:
+    # Parse the JSON data from the response
     data = response.json()
+    
+    # Extract the temperature and description from the data
     temp = data['main']['temp']
     desc = data['weather'][0]['description']
+    
+    # Print the temperature in Celsius and the weather description
     print(f"Temperature: {math.floor(temp - 273.15)} C")
     print(f"Description: {desc}")
 else: 
+    # Print an error message if the request was not successful
     print('Error fetching weather data')
+    print(f'status code: {response.status_code}')
