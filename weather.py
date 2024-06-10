93547c95e3c35c082f6c90a4347c123689953781
Jake Field <jacobprogrammes@gmail.com>
2024-06-10 15:36:15 +0100

    finished in terminal

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 import requests
-
+import math
 api_key = "de813b00c655b51d656811e5394737d2"
 
 city = input("Where would you like to see the weather for?")
@@ -7,4 +7,11 @@ city = input("Where would you like to see the weather for?")
 url = (f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
 
 response = requests.get(url)
-if response.status.code == 200
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f"Temperature: {math.floor(temp - 273.15)} C")
+    print(f"Description: {desc}")
+else: 
+    print('Error fetching weather data')
